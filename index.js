6d61af66d9c39455f6dc05342cf6c1fb7a9805da
Enzo Cipriani <egcipriani@hotmail.com>
2024-05-07 13:47:27 -0300

    finalizando

--- index.js
+++ index.js
@@ -32,3 +32,83 @@ nextButton.addEventListener('click', () => {
 
 goToSlide(currentSlide);
 
+
+var modal = document.getElementById("modal");
+var img = document.getElementById("modal-img");
+var slidesX = document.querySelectorAll(".slide img");
+
+slidesX.forEach(slide => {
+  slide.onclick = function(){
+    modal.style.display = "block";
+    img.src = this.src;
+  }
+});
+
+var span = document.getElementsByClassName("close")[0];
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+//galery 
+let apresentacoes = document.querySelectorAll('.apresentacao-fade');
+       
+        let indiceApresentacao = 1;
+        let timeoutID;
+
+        const mostrarApresentacoes = (n) => {
+            let i;
+
+            if (n > apresentacoes.length) {
+                indiceApresentacao = 1;
+            }
+            if (n < 1) {
+                indiceApresentacao = apresentacoes.length;
+            }
+
+            for (i = 0; i < apresentacoes.length; i++) {
+                apresentacoes[i].style.display = "none";
+            }
+
+
+
+            apresentacoes[indiceApresentacao - 1].style.display = 'block';
+        
+            clearTimeout(timeoutID);
+            timeoutID = setTimeout(autoApresentacoes, 4000);
+        };
+
+        const maisApresentacoes = (n) => {
+            mostrarApresentacoes(indiceApresentacao += n);
+        };
+
+        const apresentacaoAtual = (n) => {
+            mostrarApresentacoes(indiceApresentacao = n);
+        };
+
+        function autoApresentacoes() {
+            let i;
+
+            for (i = 0; i < apresentacoes.length; i++) {
+                apresentacoes[i].style.display = "none";
+            }
+
+            indiceApresentacao++;
+            if (indiceApresentacao > apresentacoes.length) {
+                indiceApresentacao = 1;
+            }
+
+            
+
+            apresentacoes[indiceApresentacao - 1].style.display = "block";
+         
+            timeoutID = setTimeout(autoApresentacoes, 4000);
+        }
+
+        autoApresentacoes();
